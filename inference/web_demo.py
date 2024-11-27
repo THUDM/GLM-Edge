38e6a89f73f3c5eeb44166b30a6c84fb2022c032
sixgod <evethwillbeok@outlook.com>
2024-11-27 19:35:41 +0800

    Update web_demo.py
    add glm-edge-v gradio support transformers

--- inference/web_demo.py
+++ inference/web_demo.py
@@ -1,4 +1,4 @@
-import os
+import time
 import argparse
 from pathlib import Path
 from threading import Thread
@@ -9,13 +9,26 @@ from PIL import Image
 
 import gradio as gr
 import torch
-from peft import AutoPeftModelForCausalLM, PeftModelForCausalLM
-from transformers import AutoModelForCausalLM, AutoTokenizer, TextIteratorStreamer, BitsAndBytesConfig
-
+from vllm import SamplingParams, AsyncEngineArgs, AsyncLLMEngine
+from peft import AutoPeftModelForCausalLM
+from transformers import (
+    AutoTokenizer,
+    AutoModelForCausalLM,
+    AutoImageProcessor,
+    TextIteratorStreamer,
+    BitsAndBytesConfig,
+)
 
 # Parse arguments
 def parse_args():
     parser = argparse.ArgumentParser(description="GLM-Edge-Chat Gradio Demo with adjustable parameters")
+    parser.add_argument(
+        "--backend",
+        type=str,
+        choices=["vllm", "transformers"],
+        required=True,
+        help="Choose inference backend: vllm or transformers",
+    )
     parser.add_argument("--model_path", type=str, required=True, help="Path to the model")
     parser.add_argument("--server_name", type=str, default="127.0.0.1", help="Server name")
     parser.add_argument("--server_port", type=int, default=7860, help="Server port")
@@ -29,14 +42,28 @@ def parse_args():
     )
     return parser.parse_args()
 
-
 args = parse_args()
-
 
 def _resolve_path(path: Union[str, Path]) -> Path:
     return Path(path).expanduser().resolve()
 
+# Load Model and Tokenizer for VLLM
+def load_vllm_model_and_tokenizer(model_dir: str, precision: str):
+    tokenizer = AutoTokenizer.from_pretrained(model_dir)
+    engine_args = AsyncEngineArgs(
+        model=model_dir,
+        tokenizer=model_dir,
+        tensor_parallel_size=1,
+        dtype="bfloat16" if precision == "bfloat16" else "float16",
+        gpu_memory_utilization=0.9,
+        enforce_eager=True,
+        worker_use_ray=True,
+        disable_log_requests=True,
+    )
+    engine = AsyncLLMEngine.from_engine_args(engine_args)
+    return engine, tokenizer
 
+# Load Model and Tokenizer for transformers
 def load_model_and_tokenizer(model_dir: Union[str, Path], precision: str, trust_remote_code: bool = True):
     model_dir = _resolve_path(model_dir)
     if precision == "int4":
@@ -60,8 +87,10 @@ def load_model_and_tokenizer(model_dir: Union[str, Path], precision: str, trust_
     tokenizer = AutoTokenizer.from_pretrained(tokenizer_dir, trust_remote_code=trust_remote_code, use_fast=False)
     return model, tokenizer
 
-
-model, tokenizer = load_model_and_tokenizer(args.model_path, args.precision, trust_remote_code=True)
+if args.backend == "vllm":
+    engine, tokenizer = load_vllm_model_and_tokenizer(args.model_path, args.precision)
+else:
+    model, tokenizer = load_model_and_tokenizer(args.model_path, args.precision, trust_remote_code=True)
 
 
 def get_image(image_path=None, image_url=None):
@@ -74,6 +103,62 @@ def get_image(image_path=None, image_url=None):
 
 
 def predict(history, prompt, max_length, top_p, temperature, image=None):
+    messages = []
+    pixel_values = None
+    if prompt:
+        messages.append({"role": "system", "content": [{"type": "text", "text": prompt}]})
+    for idx, (user_msg, model_msg) in enumerate(history):
+        if prompt and idx == 0:
+            continue
+        if idx == len(history) - 1 and not model_msg:
+            messages.append({"role": "user", "content": [{"type": "text", "text": user_msg}]})
+            break
+        if user_msg:
+            messages.append({"role": "user", "content": [{"type": "text", "text": user_msg}]})
+        if model_msg:
+            messages.append({"role": "assistant", "content": [{"type": "text", "text": model_msg}]})
+
+    if image:
+        try:
+            image_input = Image.open(image).convert("RGB")
+            messages.append({"role": "user", "content": [{"type": "text", "text": ""},{"type": "image"}]})
+        except:
+            print("Invalid image path. Continuing with text conversation.")
+        processor = AutoImageProcessor.from_pretrained(
+                args.model_path, 
+                trust_remote_code=True
+                )
+        pixel_values = torch.tensor(
+            processor(image_input).pixel_values).to("cuda")
+        
+    model_inputs = tokenizer.apply_chat_template(
+        messages, add_generation_prompt=True, tokenize=True, return_tensors="pt", return_dict=True
+    )
+    
+    streamer = TextIteratorStreamer(tokenizer, timeout=60, skip_prompt=True, skip_special_tokens=True)
+    generate_kwargs = {
+        "input_ids": model_inputs["input_ids"],
+        "attention_mask": model_inputs["attention_mask"],
+        "streamer": streamer,
+        "max_new_tokens": max_length,
+        "do_sample": True,
+        "top_p": top_p,
+        "temperature": temperature,
+        "repetition_penalty": 1.2,
+    }
+    if image:
+        generate_kwargs['pixel_values'] = pixel_values
+    else:
+        generate_kwargs['eos_token_id'] = tokenizer.encode("<|user|>")
+        
+    t = Thread(target=model.generate, kwargs=generate_kwargs)
+    t.start()
+    for new_token in streamer:
+        if new_token:
+            history[-1][1] += new_token
+        yield history
+
+async def vllm_gen(history, prompt, max_length, top_p, temperature):
     messages = []
     if prompt:
         messages.append({"role": "system", "content": prompt})
@@ -88,32 +173,20 @@ def predict(history, prompt, max_length, top_p, temperature, image=None):
         if model_msg:
             messages.append({"role": "assistant", "content": model_msg})
 
-    # Check if the model supports image input
-    if "image" in tokenizer.apply_chat_template.__code__.co_varnames:
-        messages.append({"role": "user", "content": "", "image": image})
-
-    model_inputs = tokenizer.apply_chat_template(
-        messages, add_generation_prompt=True, tokenize=True, return_tensors="pt", return_dict=True
+    inputs = tokenizer.apply_chat_template(messages, add_generation_prompt=True, tokenize=False)
+    sampling_params = SamplingParams(
+        n=1,
+        best_of=1,
+        presence_penalty=1.0,
+        frequency_penalty=0.0,
+        temperature=temperature,
+        top_p=top_p,
+        max_tokens=max_length,
     )
-    streamer = TextIteratorStreamer(tokenizer, timeout=60, skip_prompt=True, skip_special_tokens=True)
-    generate_kwargs = {
-        "input_ids": model_inputs["input_ids"],
-        "attention_mask": model_inputs["attention_mask"],
-        "streamer": streamer,
-        "max_new_tokens": max_length,
-        "do_sample": True,
-        "top_p": top_p,
-        "temperature": temperature,
-        "repetition_penalty": 1.2,
-        "eos_token_id": tokenizer.encode("<|user|>"),
-    }
-    t = Thread(target=model.generate, kwargs=generate_kwargs)
-    t.start()
-    for new_token in streamer:
-        if new_token:
-            history[-1][1] += new_token
-        yield history
-
+    async for output in engine.generate(
+        prompt=inputs, sampling_params=sampling_params, request_id=f"{time.time()}"
+    ):
+        yield output.outputs[0].text
 
 def main():
     with gr.Blocks() as demo:
@@ -148,9 +221,14 @@ def main():
 
         # Button actions and callbacks
         pBtn.click(set_prompt, inputs=[prompt_input], outputs=chatbot)
-        submitBtn.click(user, [user_input, chatbot], [user_input, chatbot], queue=False).then(
-            predict, [chatbot, prompt_input, max_length, top_p, temperature, image_input], chatbot
-        )
+        if args.backend == "vllm":
+            submitBtn.click(user, [user_input, chatbot], [user_input, chatbot], queue=False).then(
+                vllm_gen, [chatbot, prompt_input, max_length, top_p, temperature], chatbot
+            )
+        else:
+            submitBtn.click(user, [user_input, chatbot], [user_input, chatbot], queue=False).then(
+                predict, [chatbot, prompt_input, max_length, top_p, temperature, image_input], chatbot
+            )
         emptyBtn.click(lambda: (None, None), None, [chatbot, prompt_input], queue=False)
 
     demo.queue()
